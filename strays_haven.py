f60da8337d93ffabeb5a622addb756b6bfa64e1d
Donald Kiprop <kipropdonald27@gmail.com>
2025-05-13 11:04:24 +0300

    added speak method

--- strays_haven.py
+++ strays_haven.py
@@ -1,10 +1,18 @@
 class pet:
+    def speak(self):
+        print ("sound made!")
     pass
 
 class Dog:
+    def speak(self):
+        print ("bark!")
     pass
 
 class Cat:
+    def speak(self):
+        print ("meow!")
     pass
 class Rat:
+    def speak(self):
+        print ("squeak!")
     pass
